6f84eb6f50a23b49cf8b7818c9207002a2675fbb
Dan Ross <dan@rosspixelworks.com>
2014-06-27 16:13:32 -0500

    Requirements and timer_cli

--- timer.py
+++ timer.py
@@ -1,4 +1,8 @@
 from peewee import *
 from datetime import datetime
 
-
+class Timer(Model):
+    title = CharField()
+    started = DateTimeField()
+    stopped = DateTimeField()
+    total_time = DateTimeField()
---
+++ timer_cli.py
@@ -0,0 +1,11 @@
+from timer import Timer
+import click
+
+def create
+	pass
+
+def start
+	pass
+
+def stop
+	pass
