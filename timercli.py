c3d0230b66d4f0f763add9fccd89b14f30f7133d
Dan Ross <d@rossosaur.us>
2014-06-29 08:29:18 -0500

    Testing usage of click

--- timercli.py
+++ timercli.py
@@ -1,28 +1,29 @@
 from timer import Timer
 import argparse
+import click
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--create_db', help='Create the database', action='store_true')
-args = parser.parse_args()
-
-if args.create_db:
-    print("Creating database...")
-    Timer.create_table(fail_silently=True)
-    print("Created timers.db.")
+#parser = argparse.ArgumentParser()
+#parser.add_argument('--create_db', help='Create the database', action='store_true')
+#args = parser.parse_args()
+#
+#if args.create_db:
+#    print("Creating database...")
+#    Timer.create_table(fail_silently=True)
+#    print("Created timers.db.")
 
 def create(name):
     new_timer = Timer(name=name)
     new_timer.save()
 
-
+@click.group()
 def test():
     click.echo('test')
 
-
+@test.command()
 def start():
     pass
 
-
+@test.command()
 def stop():
     pass
 
