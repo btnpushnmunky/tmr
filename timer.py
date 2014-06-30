b8f613612523ba9563c20e05f8503fa9e1c99de4
Dan Ross <d@rossosaur.us>
2014-06-29 21:14:38 -0500

    Back to using Click

--- timer.py
+++ timer.py
@@ -1,13 +1,8 @@
 from peewee import *
 
-db = SqliteDatabase('timers.db')
-
 
 class Timer(Model):
     title = CharField()
     started = DateTimeField()
     stopped = DateTimeField()
     total_time = DateTimeField()
-
-    class Meta:
-        database = db
--- timercli.py
+++ timercli.py
@@ -1,30 +1,43 @@
 from timer import Timer
-import argparse
 import click
+from peewee import *
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument('--create_db', help='Create the database', action='store_true')
-#args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--create_db', help='Create the database', action='store_true')
+# args = parser.parse_args()
 #
-#if args.create_db:
+# if args.create_db:
 #    print("Creating database...")
 #    Timer.create_table(fail_silently=True)
 #    print("Created timers.db.")
-
-def create(name):
-    new_timer = Timer(name=name)
-    new_timer.save()
+DB_NAME = 'timers.db'
 
 @click.group()
-def test():
-    click.echo('test')
+def timer():
+    database = SqliteDatabase(DB_NAME)
+    database.connect()
 
-@test.command()
-def start():
-    pass
 
-@test.command()
+@timer.command(help='Create the database.')
+def init(name):
+    Timer.create_table(fail_silently=True)
+
+
+@timer.command(help='Create and start a timer.')
+@click.option("--name", prompt="Name the timer")
+def start(name):
+    new_timer = Timer(name=name)
+    new_timer.save()
+    # Mark timer as running. Probably need to provide a timer name
+    print('starting')
+
+
+@timer.command(help='Stop the timer.')
 def stop():
-    pass
+    print('stopping')
 
-    
+@timer.command(help='List running timers')
+def list():
+    print('List running timers.')
+if __name__ == "__main__":
+    timer()
