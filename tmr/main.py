3fc348b01005a2ae9c6f6ef041ef5204f9753a31
Dan Ross <dan@rosspixelworks.com>
2015-04-06 20:46:03 -0500

    Saving timers.db in user directory

--- tmr/main.py
+++ tmr/main.py
@@ -2,9 +2,14 @@
 import argparse
 from peewee import *
 from datetime import datetime
+import os
+import platform
 
+OS = platform.system()
+USER = os.getenv('LOGNAME')
+user_dir = os.environ['HOME']
 
-database = SqliteDatabase('timers.db')
+database = SqliteDatabase(os.path.join(user_dir, 'timers.db'))
 
 
 class Timer(Model):
@@ -19,7 +24,8 @@ class Timer(Model):
 
 def main():
     parser = argparse.ArgumentParser(description='Time events')
-    parser.add_argument('-i', '--init', help='Initialize the timer database')
+    parser.add_argument('-i', '--init', help='Initialize the timer database',
+            action='store_true')
     parser.add_argument('-s', '--start', help='Start a timer')
     parser.add_argument('-e', '--end', help='End a timer')
     parser.add_argument('-l', '--list', help='List running timers',
@@ -40,12 +46,14 @@ def init():
     """
     Initialize the database.
     """
-    # TODO: Add a path for the database
     Timer.create_table(fail_silently=True)
     print("Created database.")
 
 
 def start(name):
+    """
+    Start a timer with name.
+    """
     Timer.create_table(fail_silently=True)
     new_timer = Timer.create(title=name, started=datetime.now())
     new_timer.save()
@@ -53,6 +61,9 @@ def start(name):
 
 
 def stop(name):
+    """
+    Stop a timer with name.
+    """
     timer = Timer.get(Timer.title == name)
     timer.stopped = datetime.now()
     timer.total_time = (timer.stopped - timer.started).total_seconds()
@@ -62,9 +73,13 @@ def stop(name):
 
 
 def list_timers():
+    """
+    List all running timers
+    """
     timers = Timer.filter(Timer.stopped == None)
     for timer in timers:
         print("{0} has not been stopped.".format(timer.title))
+
 
 def export():
     pass
