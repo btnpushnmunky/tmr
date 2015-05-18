be335ef2fc8f9aec4db08eff94dd3d36bb0a492f
Dan Ross <dan@rosspixelworks.com>
2015-05-17 19:17:07 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='dan@rosspixelworks.com',
     license='MIT',
     packages=find_packages(),
-    install_requires=['Peewee'],
+    install_requires=['Peewee', 'tabulate'],
     entry_points={
         'console_scripts': [
             'tmr=tmr.main:main',
