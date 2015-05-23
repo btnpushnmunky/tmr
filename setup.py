83860c306bdb11887751215e0f4a8239524c904b
Dan Ross <dan@rosspixelworks.com>
2015-05-22 20:59:45 -0500

    Added tabulate to requirements

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     author_email='dan@rosspixelworks.com',
     license='MIT',
     packages=find_packages(),
-    install_requires=['Peewee'],
+    install_requires=['Peewee', 'tabulate'],
     entry_points={
         'console_scripts': [
             'tmr=tmr.main:main',
         ],
     },
-    )
+)
